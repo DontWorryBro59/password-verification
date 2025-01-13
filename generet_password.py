b67ba6a073dde0d372c7db8a01b07c5cd5edc2ea
Pertsev Sergey <pertsev.sergey59@yandex.ru>
2025-01-13 15:30:12 +0500

    Исправлены выявленные ошибки, дописан полный функционал пункта 3, выполнено интегрирование в основную программу проекта. Начало этапа тестирования

--- generet_password.py
+++ generet_password.py
@@ -1,4 +1,5 @@
 from make_cute_line import make_cute_line
+from random import choice
 
 
 def check_zero(inp: str) -> bool:
@@ -24,6 +25,7 @@ def gen_rules() -> bool | list:
     print(make_cute_line(' Введите строку согласно инструкции '))
     print(make_cute_line(' Для выхода введите 0 на любом этапе'))
     good_input = False
+
     while not good_input:
         input_rules = input()
         if check_zero(input_rules):
@@ -81,15 +83,19 @@ def gen_rules() -> bool | list:
             print('[LOG]: пользователь ввел некорректную длину строки')
             print(make_cute_line(' Ошибка ввода количества паролей '))
             print(make_cute_line(' Введите необходимое количество паролей для генерации '))
-
     rules.append(input_col)
-
     return (rules)
 
-def start_generate(rul : str) -> None:
+
+def gen_alphabit(rul: str) -> (list, int, int):
+    """
+    This function can generate options for generator
+    :param rul: str
+    :return: (list, int, int)
+    """
     alphabit = []
     if 1 in rul[:-2]:
-        for el in range(ord('а'),ord('я')):
+        for el in range(ord('а'), ord('я')):
             alphabit.append(chr(el))
     if 2 in rul[:-2]:
         for el in range(ord('А'), ord('Я')):
@@ -101,26 +107,83 @@ def start_generate(rul : str) -> None:
         for el in range(ord('A'), ord('Z')):
             alphabit.append(chr(el))
     if 5 in rul[:-2]:
-        for el in range(0,10):
+        for el in range(0, 10):
             alphabit.append(el)
     if 6 in rul[:-2]:
         for el in '!@#$%^&*(){[]}|";"~`,./?':
             alphabit.append(el)
-    print(rul)
-    print(alphabit)
+    return alphabit, rul[-2], rul[-1]
 
 
+def start_generate(alph: list, ln: int, hm: int) -> None:
+    """
+    This function can start to generate your passwords
+    :param alph:
+    :param ln:
+    :param hm:
+    :return:
+    """
+    print(make_cute_line(' Начало генерации паролей '))
+    passwords = []
+
+    for _ in range(hm):
+        password = ''
+        for _ in range(ln):
+            password += str(choice(alph))
+        passwords.append(password)
+
+    print(make_cute_line('Хотите сохранить сгенерированные пароли в файл? Введите y/n'))
+    user_input = True
+    while user_input != False:
+        user_input = input()
+        if user_input in ['y', 'n']:
+            if user_input == 'y':
+                user_input = False
+                save_to_file(passwords)
+                user_input = False
+            else:
+                user_input = False
+        else:
+            print(make_cute_line('Вы некорректно ввели ответ, введите y или n'))
+
+    print(make_cute_line('Хотите вывести сгенерированные пароли в консоль? Введите y/n'))
+    user_input = True
+    while user_input != False:
+        user_input = input()
+        if user_input in ['y', 'n']:
+            if user_input == 'y':
+                for count, el in enumerate(passwords, start=1):
+                    print(f"{count} - {el}")
+                user_input = False
+            else:
+                user_input = False
+        else:
+            print(make_cute_line('Вы некорректно ввели ответ, введите y или n'))
+
+
+def save_to_file(psw: list) -> None:
+    """
+    This function can save your passwrods list on file
+    :param psw: list()
+    :return: None
+    """
+    with open('passwords', 'w', encoding='UTF-8') as file:
+        for el in psw:
+            file.write(el + '\n')
 
 
 def main_gen_pass() -> bool:
+    """
+    This is main program in generet_password for integrate to main project program
+    :return: bool
+    """
     stop_generate = False
     while not stop_generate:
         rules = gen_rules()
         if rules == False:
-            stop_generate = True
-        start_generate(rules)
+            return False
+        alphabit, len_lst, how_much = gen_alphabit(rules)
+        start_generate(alphabit, len_lst, how_much)
+        stop_generate = True
     else:
         return False
-
-
-main_gen_pass()
